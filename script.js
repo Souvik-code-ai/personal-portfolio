ad25dab4e45643b099caf3bfc3dcf640ff3d6d6c
Souvik-code-ai <purkaitsouvik531@gmial.com>
2025-12-26 18:50:26 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var roleSwitch=new Typed(".role",{
+    strings:["Frontend Developer","Web Designer","Software Engineer","Coder"],
+    typeSpeed:80,
+    backSpeed:80,
+    backDelay:100,
+    loop:true
+})
